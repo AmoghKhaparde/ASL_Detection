9a66f6f5fea139e6cc87ece218ae7941f32548d2
AmoghKhaparde <122950756+AmoghKhaparde@users.noreply.github.com>
2023-10-13 22:50:11 -0700

    Update mod_pics.py
    This is an update to create thousands of images based on my training data to further increase the robustness of the model

--- mod_pics.py
+++ mod_pics.py
@@ -1,59 +1,70 @@
 import cv2
-import mediapipe as mp
 import os
+import torch
+from torchvision import transforms
+import numpy as np
+import matplotlib.pyplot as plt
 
-# Initialize MediaPipe Hands
-hands = mp.solutions.hands.Hands()
-mp_drawing = mp.solutions.drawing_utils
-
-for alphabet in 'abcdefghijkl' :
+def transform(image):
+    # Resize the whole image to 224 by 224
+    image = cv2.resize(image, (224, 224))
     
-    folder_path = 'test_images/'  # Replace with the path to the parent folder where you want to create the new folder
+    # Random Rotation
+    angle = np.random.uniform(-15, 15)
+    rows, cols, _ = image.shape
+    M = cv2.getRotationMatrix2D((cols / 2, rows / 2), angle, 1)
+    image = cv2.warpAffine(image, M, (cols, rows))
+
+    # Color Jitter (Contrast and Brightness)
+    # alpha = np.random.uniform(1, 10)  # Contrast factor
+    # beta = np.random.uniform(-10, 20)    # Brightness factor
+    # image = cv2.convertScaleAbs(image, alpha=alpha, beta=beta)
+
+    # Random Resized Crop
+    scale = np.random.uniform(0.5, 1.0)  # Scale factor
+    resized_rows, resized_cols = int(rows * scale), int(cols * scale)
+    image = cv2.resize(image, (resized_cols, resized_rows))
+    
+    x_start = np.random.randint(0, cols - 210)
+    y_start = np.random.randint(0, rows - 210)
+    image = image[y_start:y_start+210, x_start:x_start+210]
+
+    return image
+
+
+num_augmentations = 10
+
+for alphabet in 'abcdefgh' :
+    
+    folder_path = "new_data/" # Replace with the path to the parent folder where you want to create the new folder
     # Join the parent folder path and the new folder name to get the full path
     new_folder_path = os.path.join(folder_path, alphabet)
     # Use the os.makedirs() function to create the folder
     os.makedirs(new_folder_path)
 
-    for num in range(1, len(os.listdir('old_test_images/'+alphabet+'/'))+1) :
+    for num in range(1, len(os.listdir('pre_cropped_training_pics/'+alphabet+'/'))+1) :
         try:
             # Path to your input JPEG image
-            input_image_path = 'old_test_images/'+alphabet+'/Photo-'+str(num)+'.jpeg'
+            image_path = 'pre_cropped_training_pics/'+alphabet+'/Photo-'+str(num)+'.jpeg'
 
             # Load the input image
-            image = cv2.imread(input_image_path)
+            image = cv2.imread(image_path)
 
             # Convert the image to RGB format
-            image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            cv2.imshow('frame1', image)
 
-            # Detect hands in the image
-            results = hands.process(image_rgb)
+        
+            for i in range(num_augmentations):
+                augmented_image = transform(image)
+                # augmented_image = (np.array(augmented_image) * 255).astype(np.uint8)  # Convert to NumPy array
 
-            if results.multi_hand_landmarks:
-                # Get bounding box coordinates around the hand
-                for hand_landmarks in results.multi_hand_landmarks:
-                    x_min, y_min, x_max, y_max = float('inf'), float('inf'), 0, 0
-                    for landmark in hand_landmarks.landmark:
-                        x, y = int(landmark.x * image.shape[1]), int(landmark.y * image.shape[0])
-                        x_min = min(x_min, x)
-                        y_min = min(y_min, y)
-                        x_max = max(x_max, x)
-                        y_max = max(y_max, y)
+                cv2.imshow('frame', augmented_image)
+                cv2.waitKey(1)  # Add a delay to see the image (0 means wait indefinitely)
 
-
-                    # Crop the hand region from the original image
-                    x_min = x_min-30
-                    x_max = x_max+15
-                    y_min = y_min-15
-                    y_max = y_max+30
-                    hand_crop = image[y_min:y_max, x_min:x_max]
-
-                    # Path to the output folder
-                    output_folder = 'test_images/'+alphabet
-
-                    # Save the cropped hand image
-                    cv2.imwrite(output_folder + '/Photo-'+str(num)+'.jpeg', hand_crop)
+                # Save augmented image
+                base_filename, ext = os.path.splitext(os.path.basename(image_path))
+                output_filename = f"{base_filename}_aug_{i}{ext}"
+                output_path = os.path.join("new_data/"+alphabet+"/", output_filename)
+                cv2.imwrite(output_path, augmented_image)
         except :
             continue
-
-# Release resources
-hands.close()
